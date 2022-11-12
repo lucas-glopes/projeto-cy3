f6cfaf7e72893dcea2dbe81688e51a4481f9cbc5
Lucas Gomes <96914640+lucas-glopes@users.noreply.github.com>
2022-11-12 11:44:42 -0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,29 +1,31 @@
 // mostra barra lateral
-const iconeMenuHamburguer = document.querySelector('nav .container i.fa-bars')
-const menuHamburger = document.querySelector('.menu-hamburguer')
-const containerMenuHamburguer = menuHamburger.querySelector('.container')
+const iconeMenuHamburguer = document.querySelector("nav .container i.fa-bars");
+const menuHamburger = document.querySelector(".menu-hamburguer");
+const containerMenuHamburguer = menuHamburger.querySelector(".container");
 
-iconeMenuHamburguer.addEventListener('click', () => {
+iconeMenuHamburguer.addEventListener("click", () => {
     // aumentar opacidade do fundo
-    menuHamburger.style.visibility = 'visible'
-    menuHamburger.style.backgroundColor = 'rgba(0, 0, 0, .7)'
+    menuHamburger.style.visibility = "visible";
+    menuHamburger.style.backgroundColor = "rgba(0, 0, 0, .7)";
 
     // animar o container para dentro da tela
-    containerMenuHamburguer.style.marginRight = '0'
+    containerMenuHamburguer.style.marginRight = "0";
 
-    menuHamburguerAtivo = true
-})
+    menuHamburguerAtivo = true;
+});
 
 // ocultar barra lateral
-const iconeFecharMenuHamburguer = document.querySelector('.menu-hamburguer .container .fa-xmark')
+const iconeFecharMenuHamburguer = document.querySelector(
+    ".menu-hamburguer .container .fa-xmark"
+);
 
-iconeFecharMenuHamburguer.addEventListener('click', () => {
+iconeFecharMenuHamburguer.addEventListener("click", () => {
     // diminuir opacidade do fundo
-    menuHamburger.style.backgroundColor = 'rgba(0, 0, 0, 0)'
-    menuHamburger.style.visibility = 'hidden'
+    menuHamburger.style.backgroundColor = "rgba(0, 0, 0, 0)";
+    menuHamburger.style.visibility = "hidden";
 
     // animar o container para fora da tela
-    containerMenuHamburguer.style.marginRight = '-100%'
+    containerMenuHamburguer.style.marginRight = "-100%";
 
-    menuHamburguerAtivo = true
-})
+    menuHamburguerAtivo = true;
+});
